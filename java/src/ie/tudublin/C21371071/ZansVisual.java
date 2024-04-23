cb51e37479b48d8175d35e95d4288f589d75179e
E1setsu <c21371071@mytudublin.ie>
2024-04-23 14:35:53 +0100

    visuals update

--- java/src/ie/tudublin/C21371071/ZansVisual.java
+++ java/src/ie/tudublin/C21371071/ZansVisual.java
@@ -1,5 +1,99 @@
 package ie.tudublin.C21371071;
 
-public class ZansVisual {
+import processing.core.PApplet;
+import processing.core.PVector;
+import ddf.minim.*;
+import ddf.minim.analysis.*;
+
+public class ZansVisual extends PApplet {
+    Minim m;
+    AudioPlayer player;
+    FFT fft;
+
+    Node[] nodes = new Node[1200];
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+        m = new Minim(this);
+        player = m.loadFile("data/Joji_-_SLOW_DANCING_IN_THE_DARK.mp3", 1024);
+        fft = new FFT(player.bufferSize(), player.sampleRate());
+        player.play();
+
+        for (int i = 0; i < nodes.length; i++) {
+            nodes[i] = new Node(random(width), random(height));
+        }
+    }
+
+    public void draw() {
+        noStroke();
+        noCursor();
+        background(0);
     
+        fft.forward(player.mix);
+    
+        for (int i = 0; i < nodes.length; i++) {
+            float freq = fft.getFreq((float) (dist(nodes[i].loc.x, nodes[i].loc.y, width / 2, height / 2) * 2.2));
+    
+            // Calculate the color based on the vertical position of the node
+            float yPos = nodes[i].loc.y;
+            float gradientFactor = yPos / height; // Normalized value between 0 and 1
+            int gradientColor = lerpColor(color(255, 0, 0), color(255, 165, 0), gradientFactor); // Interpolate between red and orange
+            strokeWeight(freq / 35);
+            stroke(gradientColor);
+    
+            for (int j = i + 1; j < nodes.length; j++) {
+                Node other = nodes[j];
+                float d = nodes[i].loc.dist(other.loc);
+                if (d > 0 && d < 45) {
+                    line(nodes[i].loc.x, nodes[i].loc.y, other.loc.x, other.loc.y);
+                }
+            }
+            stroke(255);
+            nodes[i].run();
+        }
+    }
+    
+
+    class Node {
+        PVector loc;
+        PVector velocity = new PVector(random(-2, 2), random(-2, 2));
+        float size = 10;
+
+        Node(float x, float y) {
+            this.loc = new PVector(x, y);
+        }
+
+        void run() {
+            display();
+            move();
+            bounce();
+        }
+
+        void display() {
+            point(loc.x, loc.y);
+        }
+
+        void move() {
+            loc.add(velocity);
+        }
+
+        void bounce() {
+            if ((loc.x > width) || (loc.x < 0)) {
+                velocity.x *= -1;
+            }
+            if ((loc.y > height) || (loc.y < 0)) {
+                velocity.y *= -1;
+            }
+        }
+    }
+    
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"ZansVisual"};
+        ZansVisual zansVisual = new ZansVisual();
+        PApplet.runSketch(processingArgs, zansVisual);
+    }
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,14 +2,14 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 import example.AudioBandsVisual; // Add the missing import statement
-import ie.tudublin.C22338873.RachealVisual;
+import ie.tudublin.C21371071.ZansVisual;
 
 
 public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        PApplet.runSketch(a, new RachealVisual());
+        PApplet.runSketch(a, new ZansVisual());
     }
 
     public static void main(String[] args) {
